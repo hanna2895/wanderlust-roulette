9bbcf181bde89be17ffc05524f9a486f5dcc85ee
Hannah Werman <hanna.w28@gmail.com>
2019-08-12 23:00:44 +0800

    Updates

---
+++ precache-manifest.c8e8e02fcc4a7d93186e85480264ec90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f285346da377bca2db806e3514e0f4a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeacba49afeba14d220a",
+    "url": "/static/css/main.b7915607.chunk.css"
+  },
+  {
+    "revision": "b9d9f9bdd1790dcee6ca",
+    "url": "/static/js/2.a04bed9d.chunk.js"
+  },
+  {
+    "revision": "eeacba49afeba14d220a",
+    "url": "/static/js/main.90c5e254.chunk.js"
+  },
+  {
+    "revision": "a6155320185537174715",
+    "url": "/static/js/runtime~main.088625b7.js"
+  },
+  {
+    "revision": "f51b32d7580d266e620e3580c2b274d8",
+    "url": "/static/media/giphy.f51b32d7.gif"
+  },
+  {
+    "revision": "7140930f98d988de5b55b0b118463cb9",
+    "url": "/static/media/globe.7140930f.png"
+  }
+]);
