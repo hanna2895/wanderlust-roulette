861c1eca625d2c877266dc95cfc101249a873f50
Hannah Werman <hanna.w28@gmail.com>
2019-08-12 22:53:33 +0800

    basic random country generator complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// import countryData from 'country-json/src/country-by-name';
+import countryData from 'country-json/src/country-by-flag';
+
+import globe from './giphy.gif';
+import pausedGlobe from './globe.png';
+
 function App() {
+  const [animated, setAnimated] = useState(false)
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [selectedFlag, setSelectedFlag] = useState(null)
+
+  const handleClick = () => {
+    setAnimated(true)
+
+    // pick a random country from the country array
+    const countryIndex = Math.floor(Math.random() * countryData.length);
+    const country = countryData[countryIndex]
+    // set the selectedcountry and the selectedCountryFlag
+    // add a delay for dramatic effect
+    setTimeout(() => {
+      setSelectedCountry(country.country)
+      setSelectedFlag(country.flag_base64)
+      setAnimated(false)
+    }, 3000)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Wanderlust Roulette</h1>
+        <p>Spin the globe and receive a random destionation for your next vacation!</p>
+      </div>
+      <img src={animated ? globe : pausedGlobe} alt="globe" className="globe"/>
+      <div onClick={handleClick} className="spin">Spin Now!</div>
+      <hr />
+      {selectedCountry 
+        ? <>
+            <div className="header">
+              <h2>You're going to...</h2>
+              <h1>{selectedCountry}</h1>
+            </div>
+
+            <img src={selectedFlag} />
+        </>
+        : null
+      }
     </div>
   );
 }
